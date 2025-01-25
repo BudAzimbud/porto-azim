a8fe229fd9924cad1fc8a380c8c439a103789dd8
azim <budazimbud@gmail.com>
2025-01-26 02:36:50 +0800

    Commit changes

--- src/modules/home/HomeContainer.tsx
+++ src/modules/home/HomeContainer.tsx
@@ -474,7 +474,7 @@ export default function HomeContainer() {
             Tech Stack
           </h2>
 
-          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-3 sm:gap-4">
+          <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 gap-3 sm:gap-4">
             {[
               {
                 name: "React",
